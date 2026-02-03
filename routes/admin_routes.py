5c2a8d6a0d21b6f5d63bbc63c9062b489c7042f7
见人曾 <2804520536@qq.com>
2026-02-03 23:41:41 +0800

    限制图片大小，完善Base64返回功能，修复图片格式获取逻辑，并添加.gitignore过滤不必要文件

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,24 +1,41 @@
 import os
 from os import listdir
 from uuid import uuid4
-from flask import Flask,request,send_from_directory
+from flask import Flask,request,send_from_directory,jsonify
 from common.profile import Profile
 from pathlib import Path
+import base64
 
 
 app=Flask(__name__)
+app.config['MAX_CONTENT_LENGTH']=200*1024*1024
+
+def file_to_base(file):
+    data=file.read()
+    base64_data=base64.b64encode(data).decode('utf-8')
+    return base64_data
+
+@app.errorhandler(413)
+def file_too_large(error):
+    return '文件过大啦！最大支持200MB的图片捏！',413
+
 @app.route('/graph',methods=['GET','POST'])
 def upload_graph():
     graph_file =request.files.get('graph')
-
     if not graph_file:
         return '未上传有效图片'
+
     graph_path=Profile.get_graph_path()
     graph_id=str(uuid4())
     graph_suffix=Path(graph_file.filename).suffix
+
+    graph_suffix_lower=graph_suffix.lower()
+    ALLOW_SUFFIX={'.png','.jpeg','.gif','.jpg'}
+    if graph_suffix_lower not in ALLOW_SUFFIX:
+        return '文件类型错误，仅支持png、jpeg、gif、jpg文件'
     graph_filename=f'{graph_id}{graph_suffix}'
     graph_fullpath=graph_path.joinpath(graph_filename)
-    graph_file.save(graph_path.joinpath(graph_filename))
+    graph_file.save(graph_fullpath)
     return f'图片上传成功，id：{graph_id}'
 
 
@@ -32,7 +49,26 @@ def download_graph(graph_id):
             target_file=filename
             break
     if target_file:
-        return send_from_directory(up_folder,target_file)
+        return_format=request.args.get('format','stream').lower()
+
+        if return_format=='base64':
+            file_full_path=graph_path.joinpath(target_file)
+            try:
+                with open(file_full_path,'rb')as f:
+                    base64_str=file_to_base(f)
+                    return jsonify({
+                        'code':200,
+                        'msg':'获取base64成功',
+                        'data':{
+                            'graph_id':graph_id,
+                            'base64_str':base64_str,
+                            'file_format':target_file.split('.')[-1]
+                        }
+                    })
+            except Exception as e:
+                return jsonify({'code':500,'msg':f'转换Base64失败：{str(e)}'}),500
+        else:
+            return send_from_directory(up_folder,target_file)
     else:
         return '未找到对应图片'
 
